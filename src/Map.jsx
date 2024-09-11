b8f849d068579ec53451d830cc900c4fcdf601d0
Adeyemi Oyedeji <89479407+AnosVoldigoad999@users.noreply.github.com>
2024-09-11 09:30:47 +0100

    done

--- src/Map.jsx
+++ src/Map.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
+import { Icon } from 'leaflet'
 function Map({latAndLong, location}) {
+    const customIcon = new Icon({
+        iconUrl:'public/images/icon-location.svg',
+        iconSize:[30, 40]
+    })
   return <>
   <MapContainer center={latAndLong} zoom={13}> {/*the map! */}
     <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     url='https://tile.openstreetmap.org/{z}/{x}/{y}.png' />
-    <Marker position={latAndLong} > {/*za marker */}
+    <Marker position={latAndLong} icon={customIcon}> {/*za marker */}
         <Popup>{location}</Popup>
     </Marker>
   </MapContainer>
